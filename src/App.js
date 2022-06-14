946d28d9932a9aa2773fdba7a7c2aad93d0f9904
Lavulite <s.sugiyama0811@gmail.com>
2022-06-14 23:59:18 +0000

    bootstrap導入

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Navbar } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar collapseOnSelect expand = "lg" bg = "dark" variant = "dark" >
+    </Navbar >
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+const Header = () => {
+
+  return (
+    <Navbar collapseOnSelect expand = "lg" bg = "dark" variant = "dark" >
+    </Navbar >
+  );
+}
+
+export default Header;
